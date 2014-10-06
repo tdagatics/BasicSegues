14975f436ab9278106ac70db618bb358e720cfd3
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-06 05:53:14 -0700

    Initial Commit

---
+++ BasicSegues/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BasicSegues
+//
+//  Created by Anthony Dagati on 10/6/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BasicSegues/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BasicSegues
+//
+//  Created by Anthony Dagati on 10/6/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
